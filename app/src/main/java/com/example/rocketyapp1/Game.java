f9e7eba18172db6edbe90ea94fc00eb930744fec
cozmyc <conoroshields@tutanota.com>
2020-03-16 19:42:24 -0400

    Add Game.java

---
+++ app/src/main/java/com/example/rocketyapp1/Game.java
@@ -0,0 +1,17 @@
+package com.example.rocketyapp1;
+
+import java.util.List;
+
+public class Game {
+
+    private long start;
+    private List<Player> players;
+
+    public Game() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public void addPlayers(Player... players) {
+        // add da players
+    }
+}
