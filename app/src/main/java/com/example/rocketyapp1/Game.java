80f5a48c477971dff7e607c29b6f4284d72e9943
cozmyc <conoroshields@tutanota.com>
2020-03-16 20:12:48 -0400

    More game.java stuff

--- app/src/main/java/com/example/rocketyapp1/Game.java
+++ app/src/main/java/com/example/rocketyapp1/Game.java
@@ -1,17 +1,34 @@
 package com.example.rocketyapp1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
 
     private long start;
     private List<Player> players;
+    private int rounds; // we can add an option for this later
+    private Map<Long, Integer> wins;
 
     public Game() {
         this.start = System.currentTimeMillis();
+        this.players = new ArrayList<>();
+        this.wins = new HashMap<>();
     }
 
-    public void addPlayers(Player... players) {
-        // add da players
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public void addWin(Player player) {
+        // add a win to the map, if player has enough wins/rounds, complete the game
+        // and announce the winner?
+    }
+
+    public void completeGame() {
+
     }
 }
